178b2f18dc138f36e7aae1f464b7e24994d631c9
ships22 <sshipsss@gmail.com>
2021-05-01 03:16:54 +0200

    porfolio show hide filter added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// about section
+// about section -
 
 ;(() => {
   const aboutSection = document.querySelector('.about-section'),
@@ -20,6 +20,44 @@
         .classList.remove('active')
 
       aboutSection.querySelector(target).classList.add('active')
+    }
+  })
+})()
+// porfolio filter and popup -
+;(() => {
+  const filterContainer = document.querySelector('.portfolio-filter'),
+    portfolioItemsContainer = document.querySelector('.portfolio-items'),
+    portfolioItems = document.querySelectorAll('.portfolio-item'),
+    popup = document.querySelector('.portfolio-popup'),
+    prevBtn = popup.querySelector('.pp-prev'),
+    nextBtn = popup.querySelector('.pp-next'),
+    closeBtn = popup.querySelector('.pp-close'),
+    projectDetailsContainer = popup.querySelector('.pp-details'),
+    pojectDetailsBtn = popup.querySelector('.pp-project-details-btn')
+  let itemsIndex, slideIndex, screenshots
+
+  // filter portfolio items -
+  filterContainer.addEventListener('click', (event) => {
+    if (
+      event.target.classList.contains('filter-item') &&
+      !event.target.classList.contains('active')
+    ) {
+      // deactivate existing active 'filter-items' -
+      filterContainer
+        .querySelector('.active')
+        .classList.remove('outer-shadow', 'active')
+      // activate new 'filter item' -
+      event.target.classList.add('active', 'outer-shadow')
+      const target = event.target.getAttribute('data-target')
+      portfolioItems.forEach((item) => {
+        if (target === item.getAttribute('data-category') || target === 'all') {
+          item.classList.remove('hide')
+          item.classList.add('show')
+        } else {
+          item.classList.remove('show')
+          item.classList.add('hide')
+        }
+      })
     }
   })
 })()
