70f2ef7f8fdf1d9741ca3d0e9800fbf4b7ba9226
ships22 <sshipsss@gmail.com>
2021-08-14 12:08:57 +0200

    Theme started

--- js/main.js
+++ js/main.js
@@ -22,6 +22,43 @@
       document.querySelector('.fade-out-effect').classList.remove('active')
     }, 300)
   }
+  // attach an event handler to document -
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('link-item')) {
+      if (e.target.hash !== '') {
+        e.preventDefault()
+        const hash = e.target.hash
+
+        document.querySelector('.section.active').classList.add('hide')
+        document.querySelector('.section.active').classList.remove('active')
+
+        document.querySelector(hash).classList.add('active')
+        document.querySelector(hash).classList.remove('hide')
+
+        navMenu
+          .querySelector('.active')
+          .classList.add('outer-shadow', 'hover-on-shadow')
+        navMenu
+          .querySelector('.active')
+          .classList.remove('active', 'inner-shadow')
+        if (navMenu.classList.contains('open')) {
+          e.target.classList.add('active', 'inner-shadow')
+          e.target.classList.remove('outer-shadow', 'hover-on-shadow')
+          hideNavMenu()
+          console.log('here 1')
+        } else {
+          let navItems = navMenu.querySelectorAll('.link-item')
+          navItems.forEach((item) => {
+            if (hash === item.hash) {
+              item.classList.add('active', 'inner-shadow')
+              item.classList.remove('outer-shadow', 'hover-on-shadow')
+            }
+          })
+          fadeOutEffect()
+        }
+      }
+    }
+  })
 })()
 
 // about section -
